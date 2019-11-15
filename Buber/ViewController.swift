46ca6702606dedaf2c8cf58b01d7e4bac34f545f
Alexey Salangin <a7ls@ya.ru>
2019-11-15 23:35:33 +0200

    Create Xcode project

---
+++ Buber/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Buber
+//
+//  Created by Alexey Salangin on 11/15/19.
+//  Copyright © 2019 Alexey Salangin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
