16e3ac864b748756b0d2502a96963a7dc2c9d9ac
Alexey Salangin <a7ls@ya.ru>
2019-11-17 04:16:46 +0200

    Add search icon

--- Buber/SearchViewController.swift
+++ Buber/SearchViewController.swift
@@ -19,7 +19,18 @@ final class SearchViewController: UIViewController {
 
     private let whereToSearchField = UITextField()
     private let searchButton = UIButton()
+    private let searchImageView: UIImageView
     private var keyboardHeight: CGFloat = 0
+
+    init() {
+        self.searchImageView = Self.makeSearchImageView()
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,13 +44,21 @@ final class SearchViewController: UIViewController {
     }
 
     private func setupSubviews() {
+        self.view.addSubview(self.searchImageView)
         self.view.addSubview(self.whereToSearchField)
         self.view.addSubview(self.searchButton)
 
-        self.whereToSearchField.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(32)
+        self.searchImageView.snp.makeConstraints { make in
+            make.width.height.equalTo(16)
+            make.centerY.equalTo(self.searchButton)
             make.left.equalToSuperview().offset(16)
         }
+
+        self.whereToSearchField.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(32)
+            make.left.equalTo(self.searchImageView.snp.right).offset(16)
+        }
+        self.whereToSearchField.contentVerticalAlignment = .center
 
         self.searchButton.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(32)
@@ -67,5 +86,12 @@ final class SearchViewController: UIViewController {
     private func didTapSearchButton() {
         self.view.endEditing(true)
         self.delegate?.searchViewController(vc: self, searchText: self.whereToSearchField.text ?? "")
+    }
+
+    private static func makeSearchImageView() -> UIImageView {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "search")
+        imageView.tintColor = .placeholderText
+        return imageView
     }
 }
